2a41fe3fed7e01d58c2549316ae32ab12bc0cbc8
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-07 15:11:40 +0100

    re-structure

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { GoogleMapsComponent } from './components/google-maps/google-maps.component';
+import {GoogleMapsComponent} from './components/google-maps/google-maps.component';
 
 
 @NgModule({
