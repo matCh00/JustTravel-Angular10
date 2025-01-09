ff6040af302c255bc428a60c6e7d3913669066f9
mateusz.chalik <mateusz.chalik@asseco.pl>
2025-01-09 19:20:16 +0100

    multiple planes

--- app/src/app/core/header/header.component.ts
+++ app/src/app/core/header/header.component.ts
@@ -10,14 +10,28 @@ export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<boolean>();
 
 
+  planes = [
+    {src: 'assets/plane1.png'},
+    {src: 'assets/plane2.png'},
+    {src: 'assets/plane3.png'},
+    {src: 'assets/plane4.png'},
+    {src: 'assets/plane5.png'}
+  ];
+  selectedPlane: number | null = null;
+
+
   isAnimating = false;
 
   triggerPlaneAnimation() {
     if (this.isAnimating)
       return;
+
+    this.selectedPlane = Math.floor(Math.random() * this.planes.length);
+
     this.isAnimating = true;
     setTimeout(() => {
       this.isAnimating = false;
+      this.selectedPlane = null;
     }, 6000);
   }
 
